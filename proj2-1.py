cd2c2ba8119748cf2ccf2d4bde368062493df0c6
Angela-Su <i.am.angela.su@gmail.com>
2022-06-05 11:09:59 -0700

    followed guildlines from pt 1
    need to switch to specs from pt 3

--- proj2-1.py
+++ proj2-1.py
@@ -1,17 +1,44 @@
 import math
 import copy
 
+ForwardSelection():
+    print("")
+
+BackwardElim():
+
+Angela():
+
+
 def main():
     print ("Welcome to Angela Su\'s Feature Selection Algorithm.")
     # inputFile = input('What is file you want to use?') # read in data
 
     #OKAY ANGELA YOU JUST NEED TO USE YOUR BRAIN!!!
-    print ("Please enter total number of features:")
+    #print ("Please enter total number of features:")
+    numFeatures = int(input("Please enter total number of features:"))
 
-    print ("Type the number of the algorthim you want to run.")
-    print ("1) Forward Selection")
-    print ("2) Backward Elimination")
-    print ("3) Angela Special Algorithm.")
+    #change it so that get user input while asking for what algorithm to run
+    #print ("Type the number of the algorthim you want to run.")
+    #print ("1) Forward Selection")
+    #print ("2) Backward Elimination")
+    #print ("3) Angela Special Algorithm.")
 
-    user = int(input())
+    userAlg = "" #save inputs choices as strings? might be easier?
+    #make sure an algorithm is selected, loop if not
+    while (userAlg != "1" and userAlg != "2" and userAlg != "3"):
+        userAlg = input("""Type the number of the algorthim you want to run.
+                        1) Forward Selection
+                        2) Backward Elimination
+                        3) Angela Special Algorithm. \n""")
     
+    print("Beginning search.")
+
+    if(userAlg == "1"):
+        ForwardSelection()
+    elif(userAlg == "2"):
+        BackwardElim()
+    else:
+        Angela()
+
+if __name__ == '__main__':
+    main()
