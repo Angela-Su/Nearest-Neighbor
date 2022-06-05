86336b45d7b75ac49a670f2db47f6434cccaa3ef
Angela-Su <i.am.angela.su@gmail.com>
2022-06-05 09:52:09 -0700

    Create proj2-1.py

---
+++ proj2-1.py
@@ -0,0 +1,17 @@
+import math
+import copy
+
+def main():
+    print ("Welcome to Angela Su\'s Feature Selection Algorithm.")
+    # inputFile = input('What is file you want to use?') # read in data
+
+    #OKAY ANGELA YOU JUST NEED TO USE YOUR BRAIN!!!
+    print ("Please enter total number of features:")
+
+    print ("Type the number of the algorthim you want to run.")
+    print ("1) Forward Selection")
+    print ("2) Backward Elimination")
+    print ("3) Angela Special Algorithm.")
+
+    user = int(input())
+    
